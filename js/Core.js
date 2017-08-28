fe865d7bd26d8f9d51c67e4616d3c66127175903
jiajx <jiajx@mail.open.com.cn>
2017-08-28 14:18:29 +0800

    test

--- js/Core.js
+++ js/Core.js
@@ -11,6 +11,44 @@ $(document).ready(function () {
     })
 })
 
-function getRatio(a) {
-    var _this = $(a);
+function getRatio() {
+    var Goods = $("#Goods").val();
+    var Num = $("#Number").val();
+    var Start = $("#Start").val();
+    var End = $("#End").val();
+    var Ratio = $("#Ratio");
+    var Result = $("#Result");
+    var Formula = $("#Formula");
+    var StartNum , EndNum, Distance = 0;
+    if (Start == End ){
+        return false;
+    }
+    if (!Num){
+        console.log(Num);
+        Num = 0;
+    }
+    $.each(Place,function (key,val) {
+        if (val.val == Start){
+            StartNum = Start.split('-')[0];
+        }
+        if (val.val == End){
+            EndNum = End.split('-')[0];
+        }
+    })
+    $.each(RatioVal,function (a, b) {
+        if (b.key == Number(StartNum)){
+            $.each(b.to,function(key , val){
+                $.each(val.num, function(c,d){
+                    if (d == Number(EndNum)){
+                        Distance = val.val;
+                    }
+                })
+            })
+        }
+    })
+    Ratio.html(Distance);
+    Result.html(((Num/Goods)*Distance).toFixed(2));
+    Formula.html('('+Num+'/'+Goods+')*'+Distance);
 }
+
+
